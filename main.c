95dc020867e41062efee10cfa644a912c83cb66e
dev1ce <105132637+dev1ce2004@users.noreply.github.com>
2024-10-29 17:55:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 typedef struct osoba {
 	char ime[50], prezime[50];
 	int godina_rodjenja;
-	struct osoba *next;
+	struct osoba* next;
 } Osoba;
 
-Osoba* novaOsoba(char *ime, char *prezime, int godina) {
-	Osoba *nova = (Osoba*)malloc(sizeof(Osoba));
+Osoba* novaOsoba(char* ime, char* prezime, int godina) {
+	Osoba* nova = (Osoba*)malloc(sizeof(Osoba));
 	if (!nova) {
 		perror("Greska pri alokaciji memorije");
 		exit(EXIT_FAILURE);
@@ -22,39 +22,39 @@ Osoba* novaOsoba(char *ime, char *prezime, int godina) {
 	return nova;
 }
 
-void dodajNaPocetak(Osoba **head, char *ime, char *prezime, int godina) {
-	Osoba *nova = novaOsoba(ime, prezime, godina);
+void dodajNaPocetak(Osoba** head, char* ime, char* prezime, int godina) {
+	Osoba* nova = novaOsoba(ime, prezime, godina);
 	nova->next = *head;
 	*head = nova;
 }
 
-void ispisiListu(Osoba *head) {
-	for (Osoba *trenutna = head; trenutna; trenutna = trenutna->next) {
-		printf("%s %s %d\n", trenutna->ime, trenutna->prezime, trenutna->godina_rodjenja);
+void ispisiListu(Osoba* head) {
+	for (Osoba* trenutna = head; trenutna; trenutna = trenutna->next) {
+		printf("\n\n%s %s %d\n\n", trenutna->ime, trenutna->prezime, trenutna->godina_rodjenja);
 	}
 }
 
-void dodajNaKraj(Osoba **head, char *ime, char *prezime, int godina) {
-	Osoba *nova = novaOsoba(ime, prezime, godina);
+void dodajNaKraj(Osoba** head, char* ime, char* prezime, int godina) {
+	Osoba* nova = novaOsoba(ime, prezime, godina);
 	if (!*head) {
 		*head = nova;
 	}
 	else {
-		Osoba *trenutna = *head;
+		Osoba* trenutna = *head;
 		while (trenutna->next) trenutna = trenutna->next;
 		trenutna->next = nova;
 	}
 }
 
-Osoba* pronadjiPoPrezimenu(Osoba *head, char *prezime) {
-	for (Osoba *trenutna = head; trenutna; trenutna = trenutna->next) {
+Osoba* pronadjiPoPrezimenu(Osoba* head, char* prezime) {
+	for (Osoba* trenutna = head; trenutna; trenutna = trenutna->next) {
 		if (strcmp(trenutna->prezime, prezime) == 0) return trenutna;
 	}
 	return NULL;
 }
 
-void brisiOsobu(Osoba **head, char *prezime) {
-	Osoba *trenutna = *head, *prethodna = NULL;
+void brisiOsobu(Osoba** head, char* prezime) {
+	Osoba* trenutna = *head, * prethodna = NULL;
 	while (trenutna && strcmp(trenutna->prezime, prezime)) {
 		prethodna = trenutna;
 		trenutna = trenutna->next;
@@ -70,7 +70,7 @@ void brisiOsobu(Osoba **head, char *prezime) {
 }
 
 int main() {
-	Osoba *head = NULL;
+	Osoba* head = NULL;
 	int izbor;
 	char ime[50], prezime[50], buffer[100];
 	int godina_rodjenja;
@@ -96,11 +96,11 @@ int main() {
 			sscanf(buffer, "%s %s %d", ime, prezime, &godina_rodjenja);
 			dodajNaKraj(&head, ime, prezime, godina_rodjenja);
 			break;
-		case 4:
+		case 4: {
 			printf("Unesite prezime: ");
 			fgets(prezime, sizeof(prezime), stdin);
 			prezime[strcspn(prezime, "\n")] = 0;
-			Osoba *pronadjena = pronadjiPoPrezimenu(head, prezime);
+			Osoba* pronadjena = pronadjiPoPrezimenu(head, prezime);
 			if (pronadjena) {
 				printf("Pronadjena osoba: %s %s %d\n", pronadjena->ime, pronadjena->prezime, pronadjena->godina_rodjenja);
 			}
@@ -108,6 +108,7 @@ int main() {
 				printf("Osoba s prezimenom %s ne postoji\n", prezime);
 			}
 			break;
+		}
 		case 5:
 			printf("za brisat: ");
 			fgets(prezime, sizeof(prezime), stdin);
@@ -120,10 +121,11 @@ int main() {
 	} while (izbor != 0);
 
 	while (head) {
-		Osoba *temp = head;
+		Osoba* temp = head;
 		head = head->next;
 		free(temp);
 	}
 
 	return 0;
 }
+
